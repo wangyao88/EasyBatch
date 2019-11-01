0aece98a851a8665563b4eac53a6b6449ce35759
wangyao <wangyao@cis.com.cn>
2019-11-01 16:05:14 +0800

    项目初始化

---
+++ src/main/java/com/mohan/project/easy/batch/EasyBatchApplication.java
@@ -0,0 +1,13 @@
+package com.mohan.project.easy.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBatchApplication.class, args);
+	}
+
+}
